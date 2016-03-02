33196b47fd720c76fdd96adb227d1405b082aa0a
Gwyn Jones <gwyn.jones@digital.justice.gov.uk>
2016-03-02 21:15:22 +0000

    Add response differentiation by name
    Adds the functionality for different responses based on names.
    Introduces an array of 'friends' and an array of 'foes' to achieve
    this.

--- exercise.rb
+++ exercise.rb
@@ -1,14 +1,31 @@
 class SayHello
 
-  attr_accessor :name
-  
+  attr_accessor :name, :response
+
+  def respond_to_name(name)
+    case true
+      when @friends.include?(name)
+        'Great to see you again'
+      when @foes.include?(name)
+        'Wish I hadn\'t met you'
+      else
+        'Nice to meet you'
+    end
+  end
+
   def greet
-    "Hey, my name is Ruby. Nice to meet you #{@name}."
+    @response = respond_to_name(@name)
+    "Hey, my name is Ruby. #{@response} #{@name}."
   end
 
   def prompt_for_name
     puts "Hey, what's your name?"
     @name = gets
+  end
+
+  def initialize
+    @friends = ['Matt']
+    @foes = ['Judas']
   end
 
 end
--- unit_tests.rb
+++ unit_tests.rb
@@ -11,9 +11,19 @@ class SayHelloTest < Minitest::Test
     assert_nil @greeter.name
   end
 
-  def test_greet
-    @greeter.name = 'Overjoyed'
-    assert_equal 'Hey, my name is Ruby. Nice to meet you Overjoyed.', @greeter.greet
+  def test_greet_friend
+    @greeter.name = 'Matt'
+    assert_equal 'Hey, my name is Ruby. Great to see you again Matt.', @greeter.greet
+  end
+
+  def test_greet_foe
+    @greeter.name = 'Judas'
+    assert_equal 'Hey, my name is Ruby. Wish I hadn\'t met you Judas.', @greeter.greet
+  end
+
+  def test_greet_unknown
+    @greeter.name = 'Bob'
+    assert_equal 'Hey, my name is Ruby. Nice to meet you Bob.', @greeter.greet
   end
 
 end
